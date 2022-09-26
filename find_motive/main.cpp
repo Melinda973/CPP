6e87c1647689bb03d88fd61bd58a70ddd3e48bed
Melinda Marmot <melinda.marmot@epita.fr>
2022-09-26 22:13:26 +0200

    exercise2_first

---
+++ find_motive/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iosfwd>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc , char *argv[])
+{
+    ifstream file(argv[1]);
+    if (file.fail())
+    {
+        cout << "The file " << argv[1] << " could not be opened.\n" << endl;
+        return 1;
+    }
+
+    string word;
+    int x = 0;
+
+    while(file >> word)
+    {
+       if (word.find(argv[2]) != string::npos)
+            x++;
+    }
+    cout << "The file " << argv[1] << " contains "<< x << " words containing the motive " << argv[2] << endl;
+    return 0;
+}
